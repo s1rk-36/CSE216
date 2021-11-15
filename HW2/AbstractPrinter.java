4044b65a8ce50df3d9293f4fd483eb1c074ab71b
s1rk-36 <79171927+s1rk-36@users.noreply.github.com>
2021-11-15 18:19:04 -0500

    Delete HW2 directory

--- HW2/AbstractPrinter.java
+++
@@ -1,6 +0,0 @@
-/**
- * DO NOT CHANGE THIS CODE
- */
-public abstract class AbstractPrinter<T> {
-    abstract void print(T t);
-}
--- HW2/Printer.java
+++
@@ -1,9 +0,0 @@
-/**
- * DO NOT CHANGE THIS CODE
- */
-public class Printer<TwoDShape> extends AbstractPrinter<TwoDShape> {
-    @Override
-    void print(TwoDShape s) {
-        System.out.println(s);
-    }
-}
